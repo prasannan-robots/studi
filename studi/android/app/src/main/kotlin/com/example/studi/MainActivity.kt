f0454a8848220433de00170643073e44e5b40acf
Prasanna <prasannanatarajan3595@gmail.com>
2023-06-22 11:49:47 +0530

    Initialisation

---
+++ studi/android/app/src/main/kotlin/com/example/studi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- v_nex/android/app/src/main/kotlin/com/example/v_nex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.v_nex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
